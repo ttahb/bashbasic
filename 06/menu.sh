e8abcf9748556d9b37ba20461bd154b0ca34c288
Vijay Bhatt <bhattvijay69@hotmail.com>
2021-11-28 20:46:33 +0530

    added create user and delete user

---
+++ 06/menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while true ; do
+   clear
+   echo "Choose 1,2 or 3"
+   echo "1: Logged in users"
+   echo "2: Date in 90 days"
+   echo "3: Quit"
+   read -sn1
+   case "$REPLY" in 
+     1) who;;
+     2) date --date "90 days";;
+     3) exit 0;;
+     *) echo "Selection not recognized, please choose again";;
+   esac
+   read -n1 -p "Press any key to continue"
+done
